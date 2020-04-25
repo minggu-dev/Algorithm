636f8b493a070e99ee27535428460a9fa1a6529a
KimMinHo <rkawk6633@gmail.com>
2020-04-26 03:15:07 +0900

    프로그래머스 Level1 알고리즘 문제 정리

---
+++ src/programmers/Level1/AppendString.java
@@ -0,0 +1,29 @@
+package programmers.Level1;
+
+public class AppendString {
+	/**
+	 * 문자열 n만큼 이어붙이기
+	 * @param n
+	 * @return
+	 */
+	/*3항연산자 사용
+	public String solution(int n) {
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+				builder.append(i % 2 == 0 ? "수" : "박");
+		}
+		return builder.toString();
+	}
+	*/
+	//2개씩 이어 붙이기
+	public String solution(int n) {
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < n/2; i++) {
+				builder.append("수박");
+		}
+        if(n % 2 == 1){
+            builder.append("수");
+        }
+		return builder.toString();
+	}
+}
