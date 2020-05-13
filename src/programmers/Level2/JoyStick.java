e73d1c91c38b7c4b6876b3c839a051d7102c1687
MINHO-KIM-PROGRAMMER <61855672+MINHO-KIM-PROGRAMMER@users.noreply.github.com>
2020-05-13 09:36:49 +0900

    프로그래머스 Level2. 조이스틱

---
+++ src/programmers/Level2/JoyStick.java
@@ -0,0 +1,41 @@
+package programmers.Level2;
+
+import java.util.LinkedList;
+
+public class JoyStick {
+	/**
+	 * 조이스틱을 움직여서 알파벳 이름을 완성시키기
+	 * 출처 : https://programmers.co.kr/learn/challenges
+	 * @param name
+	 * @return
+	 */
+	public int solution(String name) {
+		int answer = (name.charAt(0) - 'A') > 12 ? 26 - (name.charAt(0) - 'A') : name.charAt(0) - 'A';
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(0);
+		
+		for(int i = 1; i < name.length(); i++) {
+			if(name.charAt(i) != 'A') {
+				list.add(i);
+				answer += (name.charAt(i) - 'A') > 12 ? 26 - (name.charAt(i) - 'A') : name.charAt(i) - 'A';
+			}
+		}
+		answer += findCursor(list, name.length());
+		return answer;
+	}
+	
+	public int findCursor(LinkedList<Integer> list, int len) {
+		int min = Integer.MAX_VALUE;
+		int size = list.size();
+		//오른쪽부터
+		for(int i = 1; i < size - 1; i++) {
+			min = Math.min(min, list.get(size - i - 1) + (len - list.get((size - i) % size)) % len + Math.min(list.get(size - i - 1), (len - list.get((size - i) % size)) % len));
+		}
+		return min;
+	}
+	
+	public static void main(String[] args) {
+		JoyStick js = new JoyStick();
+		System.out.println(js.solution("Z"));
+	}
+}
