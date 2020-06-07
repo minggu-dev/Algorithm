d34812f0976ce8acb2765c5e9abcb87e49d3fe97
KimMinHo <rkawk6633@gmail.com>
2020-06-07 22:59:42 +0900

    프로그래머스 Level2. 행렬의 곱셈

---
+++ src/programmers/Level2/MetrixMultiple.java
@@ -0,0 +1,19 @@
+package programmers.Level2;
+
+public class MetrixMultiple {
+	/**
+	 * 두 행렬의 곱셈 결과를 출력
+	 * 출처 : https://programmers.co.kr/learn/challenges
+	 */
+	public int[][] solution(int[][] arr1, int[][] arr2) {
+		int[][] answer = new int[arr1.length][arr2[0].length];
+		for(int i = 0; i < arr1.length; i++) {
+			for(int j = 0; j < arr2[0].length; j++) {
+				for(int k = 0; k < arr1[i].length; k++) {
+					answer[i][j] += arr1[i][k] * arr2[k][j];
+				}
+			}
+		}
+		return answer;
+	}
+}
